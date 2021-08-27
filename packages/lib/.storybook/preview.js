230443e1579cd37abc310014f2979c5158b84eb9
Henri <henri@ThinkPad-X260-e603803b.localdomain>
2021-08-27 13:15:24 +0200

    Setup storybook and nextjs with scss and typescript support

---
+++ packages/lib/.storybook/preview.js
@@ -0,0 +1,38 @@
+import * as nextImage from 'next/image';
+import '../styles/global.scss';
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+}
+
+// Replace next/image for Storybook
+Object.defineProperty(nextImage, 'default', {
+  configurable: true,
+  value: (props) => {
+    const { width, height } = props
+    const ratio = (height / width) * 100
+    return (
+        <div
+            style={{
+              paddingBottom: `${ratio}%`,
+              position: 'relative',
+            }}>
+          <img
+              style={{
+                objectFit: 'cover',
+                position: 'absolute',
+                width: '100%',
+                height: '100%',
+              }}
+              {...props}
+          />
+        </div>
+    )
+  },
+})
---
+++ packages/lib/README.md
@@ -0,0 +1,3 @@
+Based on:
+https://medium.com/swlh/2020-complete-setup-for-storybook-nextjs-typescript-scss-and-jest-1c9ce41e6481
+https://javascript.plainenglish.io/next-js-storybook-3f8e5a8dab3d
